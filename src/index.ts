75ddc0601aa795c2f0310326a30b60f6c5289a3e
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-12-08 17:37:00 -0800

    expose more functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { getSealHubValidatorInputs } from './zk-inputs/getSealHubValidatorInputs'
 export { getSealHubInputs } from './zk-inputs/getSealHubInputs'
 
+export { getCommitmentFromPrecommitment } from './commitments/getCommitmentFromPrecommitment'
 export { getCommitmentFromSignature } from './commitments/getCommitmentFromSignature'
 export { isCommitmentRegistered } from './commitments/isCommitmentRegistered'
 
@@ -10,5 +11,6 @@ export { getMessageForAddress } from './helpers/getMessageForAddress'
 export { Mimc7 } from './helpers/Mimc7'
 export { fetchAllCommitments } from './helpers/fetchAllCommitments'
 export { connectSealHub } from './helpers/connectSealHub'
+export { getMerkleTreeInputs } from './helpers/getMerkleTreeInputs'
 
 export { Precommitment } from './models/Precommitment'
