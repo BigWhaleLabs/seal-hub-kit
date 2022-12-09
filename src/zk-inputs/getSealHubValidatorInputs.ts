1750cc521f06a5ad485a2c6da2c9aeffafd095f9
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-12-08 17:05:14 -0800

    fix the main function

--- src/zk-inputs/getSealHubValidatorInputs.ts
+++ src/zk-inputs/getSealHubValidatorInputs.ts
@@ -2,7 +2,8 @@ import { Provider } from '@ethersproject/abstract-provider'
 import { fetchAllCommitments } from '../helpers/fetchAllCommitments'
 import { getCommitmentFromPrecommitment } from '../commitments/getCommitmentFromPrecommitment'
 import { getMerkleTreeInputs } from '../helpers/getMerkleTreeInputs'
-import { getPrecomputesFromSignature } from '../helpers/getTPreComputesFromSignature'
+import { getTPreComputesFromSignature } from '../helpers/getTPreComputesFromSignature'
+import { getUAndSFromSignature } from '..'
 import { isCommitmentRegistered } from '../commitments/isCommitmentRegistered'
 import { utils } from 'ethers'
 
@@ -11,13 +12,9 @@ export async function getSealHubValidatorInputs(
   message: string,
   provider: Provider
 ) {
-  // Getting precomputes here because we need to export them
-  const precomputes = getPrecomputesFromSignature(signature, message)
+  const { U, s } = getUAndSFromSignature(signature, message)
   const address = utils.verifyMessage(message, signature)
-  const commitment = await getCommitmentFromPrecommitment({
-    ...precomputes,
-    address,
-  })
+  const commitment = await getCommitmentFromPrecommitment({ U, s, address })
   // Check if commitment is registered
   if (!(await isCommitmentRegistered(commitment, provider))) {
     throw new Error(`Commitment: ${commitment} not found in SealHub!`)
@@ -25,9 +22,13 @@ export async function getSealHubValidatorInputs(
   // Fetch all commitments and construct the Merkle tree
   const commitments = await fetchAllCommitments(provider)
   const merkleTreeInputs = await getMerkleTreeInputs(commitment, commitments)
+  // Get precomputes
+  const precomputes = getTPreComputesFromSignature(signature)
   // Return the result
   return {
     ...precomputes,
+    U,
+    s,
     ...merkleTreeInputs,
     commitmentMerkleRootIndex: commitments.length - 1,
   }
