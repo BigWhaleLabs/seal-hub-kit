53d8b5a6c084228a0cf1a40b82f773b8651676b7
sarmat <skhachirov12@gmail.com>
2022-12-12 14:20:04 -0300

    add SealHubChecker exportable contract

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from 'hardhat/config'
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      outputSelection: {
+        '*': {
+          '*': ['storageLayout'],
+        },
+      },
+    },
+  },
+}
+
+export default config
